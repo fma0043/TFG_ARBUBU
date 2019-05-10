c8fd78e853d1b9fe47bcb4d95195507acfc6310a
fma0043 <fma0043@alu.ubu.es>
2019-05-10 12:03:16 +0200

    modificaciones

--- Proyecto/arbubu/aplicaciones/pantallaPrincipal/models.py
+++ Proyecto/arbubu/aplicaciones/pantallaPrincipal/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+#import crypt
 
 # Create your models here.
 familias=( ('AC','Aceráceas'), ('AN','Anacardiáceas'), ('AQ','Aquifoliáceas'), ('BE','Betuláceas'), ('BU','Buxáceas'), ('CA','Caprifoliáceas'),('CE','Celastráceas'),('CO','Cornáceas'),('CU','Cupresáceas'),('EL','Eleagnáceas'),('ER','Ericácas'),('FA','Fagáceas'),('JU','Juglandáceas'),('LA','Lauráceas'),('LE','Leguminosas'),('ME','Meliáceas'),('MI','Mirtáceas'),('MO','Moráceas'),('OL','Oleáceas'),('PA','Palmáceas'),('PI','Pináceas'),('PL','Platanáceas'),('PU','Punicáceas'),('RA','Ramnáceas'),('RO','Rosáceas'),('SA','Salicáceas'),('SI','Simaroubáceas'),('SO','Solanáceas'),('TA','Tamariacáceas'),('TA','Taxáceas'),('TI','Tiliáceas'),('UL','Ulmáceas'),)
@@ -31,7 +32,7 @@ class Individuos(models.Model):
     motivoSingular = models.TextField('Motivo de Singularidad',choices=motivo_singularidad, blank=False)
     x = models.IntegerField('X',blank=False, default=1)
     y = models.IntegerField('Y',blank=False,default=1)
-    tipoEspecie = models.ForeignKey(Especie, on_delete=models.CASCADE, blank=False, default=1)
+    tipoEspecie = models.ForeignKey(Especie, on_delete=models.CASCADE, blank=False)
     fotoArbol = models.ImageField(upload_to='static/imagenes',blank=False)
     fotoHojas = models.ImageField(upload_to='static/imagenes',blank=True)
     fotoTronco = models.ImageField(upload_to='static/imagenes',blank=True)
@@ -43,7 +44,16 @@ class Individuos(models.Model):
             return self.nombre
 
 class Usuario(models.Model):
-    nombre = models.CharField('Nombre',unique=True,blank=False,max_length=30)
+    nombre = models.CharField('Nombre',blank=False,max_length=30)
+    primerApellido = models.CharField('Primer Apellido',blank=False,max_length=30,default="")
+    segundoApellido = models.CharField('Segundo Apellido',blank=False,max_length=30,default="")
+    email = models.EmailField('E-mail',default="")
+    contrasenia = models.CharField('Contraseña',blank=False,max_length=30,default="")
+
 
     def __str__(self):
             return self.nombre
+
+    def encriptarContrasenia(self):
+        contraseniaEncriptada=crypt.crypt(self.contrasenia, 'salt')
+        return contraseniaEncriptada
